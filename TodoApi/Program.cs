9932f8446227e89f07d2791859ff36d0d4fb2124
USER <USER@DESKTOP-NA8K5F4>
2023-04-07 17:21:41 +0800

    Add project files.

---
+++ TodoApi/Program.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/todoitems", async (TodoDb db) => await db.Todos.ToListAsync());
+
+app.MapGet("/todoitems/complete", async (TodoDb db) =>
+    await db.Todos.Where(t => t.IsComplete).ToListAsync());
+
+app.MapGet("/todoitems/{id}", async (int id, TodoDb db) =>
+    await db.Todos.FindAsync(id)
+        is Todo todo
+            ? Results.Ok(todo)
+            : Results.NotFound()
+    );
+
+
+
+
+app.Run();
---
+++ TodoApi/Todo.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoApi
+{
+    public class Todo
+    {
+        public string Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
