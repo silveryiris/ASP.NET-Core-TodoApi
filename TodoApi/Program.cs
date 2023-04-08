1e61b176b058447edf48ea583fa23169c6a75200
Wenwei Chang <silveryiris@gmail.com>
2023-04-08 18:55:45 +0800

    feat: 新增todo item api CRUD endpoints

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -20,7 +20,39 @@ app.MapGet("/todoitems/{id}", async (int id, TodoDb db) =>
             : Results.NotFound()
     );
 
+app.MapPost("/todoitems", async (Todo todo, TodoDb db) =>
+{
+    db.Todos.Add(todo);
+    await db.SaveChangesAsync();
 
+    return Results.Created($"todoitems/{todo.Id}", todo);
+});
+
+app.MapPut("/todoitems/{id}", async (int id, Todo todo, TodoDb db) =>
+{
+    var savedTodo = await db.Todos.FindAsync(id);
+
+    if (savedTodo is null) return Results.NotFound();
+
+    savedTodo.Name = todo.Name;
+    savedTodo.IsComplete = todo.IsComplete;
+
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+});
+
+app.MapDelete("/todoitems/{id}", async (int id, TodoDb db) =>
+{
+    if (await db.Todos.FindAsync(id) is Todo todo)
+    {
+        db.Todos.Remove(todo);
+        await db.SaveChangesAsync();
+        return Results.Ok(todo);
+    }
+
+    return Results.NotFound();
+});
 
 
 app.Run();
